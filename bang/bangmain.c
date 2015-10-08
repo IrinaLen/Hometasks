310a7eb611d0d1954f2d3964d70ca2757d68e50c
IrinaLen <ia.len@mail.ru>
2015-10-08 18:19:31 +0300

    bang

---
+++ bang/bangmain.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int bang (int x)
+{int invx = ~x;
+ int negx = invx + 1;
+ return ((~negx & invx)>>31) & 1;
+
+}
+
+int main()
+{
+   int x,y;
+ scanf("%d",&x);
+ printf("%d\n",bang(x));
+    return 0;
+}
