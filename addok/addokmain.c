1822f38055048c79d19bdb2c1227a37ea228bebb
IrinaLen <ia.len@mail.ru>
2015-10-15 19:27:45 +0300

    try again

--- addok/addokmain.c
+++ addok/addokmain.c
@@ -3,21 +3,12 @@
 
 int addOk (int x, int y)
 {
-    int a = 255, x1, y1, z, z1, b;
-    b = (a << 7) | a;
-    a = (a << 8) | a;
-    x1 = a & x;
-    y1 = a & y;
-    z = x1 + y1;
-    z = z >> 16;
-    x1 = x >> 16;
-    y1 = y >> 16;
-    x1 = x1 & b;
-    y1 = y1 & b;
-    z1 = x1 + y1;
-    z1 = z1 + z;
-    z1 >>= 15;
-    return (!z1);
+    int s, x1, y1, itog;
+    s = x + y;
+    x1 = x >> 31;
+    y1 = y >> 31;
+    itog = s >> 31;
+    return !((x1 ^ itog) & (y1 ^ itog));
 }
 
 int main()
