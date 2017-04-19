e8843c2ba623a5d9179b489e0ca2effb6a8d54c6
Irina Len <ia.len@mail.ru>
2017-04-19 23:49:36 +0300

    clear branch

--- Stream_lessons_old/Assets/Scripts/UI/Menu/Craft.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Craft : MonoBehaviour
-{
-	private List<Receipt> usualReceipts = new List<Receipt>();
-    private List<Receipt> doctorReceipts = new List<Receipt>();
-}
